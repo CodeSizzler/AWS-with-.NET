7c57cd9d95b04f21cfcfb590356c24ff16967460
naaveenvaddey21-droid <naaveen.vaddey21@gmail.com>
2025-09-18 20:21:06 +0530

    Update Program.cs
    added connection string from appconfig to avoid 500 error

--- amazon-sqs-for-dotnet-developers-main/Consumer.API/Program.cs
+++ amazon-sqs-for-dotnet-developers-main/Consumer.API/Program.cs
@@ -4,10 +4,19 @@ using Consumer.API;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
-builder.Services.AddAWSService<IAmazonSQS>();
+//builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+//builder.Services.AddAWSService<IAmazonSQS>();
+var awsOptions = builder.Configuration.GetSection("AWS");
+var accessKey = awsOptions["AccessKey"];
+
+var secretKey = awsOptions["SecretKey"];
+
+var region = awsOptions["Region"];
+// Register the AWS S3 client with credentials
+builder.Services.AddSingleton<IAmazonSQS>(sp =>
+    new AmazonSQSClient(accessKey, secretKey, Amazon.RegionEndpoint.GetBySystemName(region))
+);
 builder.Services.AddHostedService<OrderCreatedEventConsumer>();
 var app = builder.Build();
 
