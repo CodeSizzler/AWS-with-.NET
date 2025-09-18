ef8d1c71b19b75d75c033685653ed0c92fa3b848
Code$izzler <30734357+CodeSizzler@users.noreply.github.com>
2025-09-18 21:56:26 +0530

    Add files via upload

---
+++ AWS CDK/src/CdkForDotnetDevelopers/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.CDK;
+
+namespace CdkForDotnetDevelopers
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CdkForDotnetDevelopersStack(app, "CdkForDotnetDevelopersStack", new StackProps
+            {
+            });
+            app.Synth();
+        }
+    }
+}
